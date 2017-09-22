0e2ac262ea08116ba679bf868b288660db7b0141
mparker <mparker@pivotal.io>
2017-09-22 15:49:43 -0400

    first four presentation tests passing

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -8,7 +8,23 @@ class PlayForm extends React.Component {
     }
 
     submitForm() {
+        this.props.rps.play("p1 throw", "p2 throw", this)
+    }
+
+    tie(){
         this.setState({message: "TIE"})
+    }
+
+    p1Wins(){
+        this.setState({message: "P1 Wins!"})
+    }
+
+    p2Wins(){
+        this.setState({message: "P2 Wins!"})
+    }
+
+    invalid(){
+        this.setState({message: "INVALID!!!!"})
     }
 
     render() {
@@ -35,6 +51,55 @@ describe("play form", function () {
             expect(page()).toContain("TIE")
         })
 
+    })
+
+    describe("when the game logic declares p1 wins", function () {
+        beforeEach(function () {
+            mountApp({
+                play(p1Throw, p2Throw, observer){
+                    observer.p1Wins()
+                }
+            })
+        })
+
+        it("displays 'P1 Wins!'", function () {
+            expect(page()).not.toContain("P1 Wins!")
+            submitForm()
+            expect(page()).toContain("P1 Wins!")
+        })
+
+    })
+
+    describe("when the game logic declares p2 wins", function () {
+        beforeEach(function () {
+            mountApp({
+                play(p1Throw, p2Throw, observer){
+                    observer.p2Wins()
+                }
+            })
+        })
+
+        it("displays 'P2 Wins!'", function () {
+            expect(page()).not.toContain("P2 Wins!")
+            submitForm()
+            expect(page()).toContain("P2 Wins!")
+        })
+    })
+
+    describe("when the game logic declares invalid", function () {
+        beforeEach(function () {
+            mountApp({
+                play(p1Throw, p2Throw, observer){
+                    observer.invalid()
+                }
+            })
+        })
+
+        it("displays 'INVALID!!!!'", function () {
+            expect(page()).not.toContain("INVALID!!!!")
+            submitForm()
+            expect(page()).toContain("INVALID!!!!")
+        })
     })
 
     let domFixture
